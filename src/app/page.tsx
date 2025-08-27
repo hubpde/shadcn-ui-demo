7ccf1b765460c72ca782ccadd89dd55a903a5d52
hubpde <github@hubp.de>
2025-08-27 19:49:04 +0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -153,7 +153,8 @@ export default function Home() {
   };
 
   const playEpisode = (url: string, index: number) => {
-    setCurrentVideoUrl(`https://mzm3u8jx.com/aliplayer.html?url=${url}`);
+    // setCurrentVideoUrl(`https://mzm3u8jx.com/aliplayer.html?url=${url}`);
+    setCurrentVideoUrl(`https://tools.liumingye.cn/m3u8/#${url}`);
     setCurrentEpisodeIndex(index);
   };
 
